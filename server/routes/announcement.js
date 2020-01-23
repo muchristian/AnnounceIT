b30ca9fbfca5fc9ccd5b514db1f9f8bd39f6ab40
muchristian <mucyochristian2@gmail.com>
2020-01-23 13:43:19 +0200

    feat(update announcement):User advertiser can update announcement endpoint
    User advertiser should be able to update announcement text with this api endpoint
    
    [Finishes #170865835]

--- server/routes/announcement.js
+++ server/routes/announcement.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
-import { createAnnounce } from '../controllers/announcement';
+import { createAnnounce, updateAnnounce } from '../controllers/announcement';
 import { validateAnnounces } from '../middleware/validateData'
 import { userVerify } from '../middleware/authToken';
 
 const route = Router();
 
 route.post(`/announcement`, [userVerify, validateAnnounces], createAnnounce);
+route.put(`/announcement/:id/`, [userVerify, validateAnnounces], updateAnnounce);
 
 export default route;
