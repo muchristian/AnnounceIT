a1058caa711db93c29683c52810b6312d5308564
muchristian <mucyochristian2@gmail.com>
2020-01-28 11:31:00 +0200

    well test

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
 import cors from './config/cors';
 import constants from './config/constants';
-import routes from './routes';
+//import routes from './routes';
 
 const server = express();
 server.use(bodyParser.urlencoded({ extended: false }));
@@ -13,7 +13,7 @@ server.use(bodyParser.json());
 server.use(morgan('dev'));
 server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 server.use(cors);
-routes(server);
+//routes(server);
 server.listen(constants.PORT, async (req, res) =>{
     try{
       console.log(`server start on port:${constants.PORT}`);
