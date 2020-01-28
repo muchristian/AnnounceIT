867c4e87984432add5311ca7462a127c601e9140
mucyo chris <mucyochristian2@gmail.com>
2020-01-28 10:15:01 +0200

    index update

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ server.use(bodyParser.json());
 server.use(morgan('dev'));
 server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 server.use(cors);
-routes(server);
+//routes(server);
 server.listen(constants.PORT, async (req, res) =>{
     try{
       console.log(`server start on port:${constants.PORT}`);
